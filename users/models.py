d86136d7f6e293b69c2ded33473dd2cba304485f
magsyg <56266980+magsyg@users.noreply.github.com>
2020-10-06 21:20:48 +0200

    Create user system

--- users/models.py
+++ users/models.py
@@ -4,5 +4,23 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 
-#class User(AbstractBaseUser, PermissionsMixin):
+from .managers import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=12, unique=True)
+    SECTIONS = [
+        ('SH', 'Shooter'),
+        ('TR', 'Trooper'),
+        ('PL', 'Pilot'),
+        ('GR', 'General')
+    ]
+    section = models.CharField(_('section'),max_length=2, choices=SECTIONS, default='TR')
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['sections']
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
