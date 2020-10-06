12972084073db68f33ede31ca21a2ac58fc4a123
magsyg <56266980+magsyg@users.noreply.github.com>
2020-10-06 18:54:47 +0200

    fix add

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+#class User(AbstractBaseUser, PermissionsMixin):
+
